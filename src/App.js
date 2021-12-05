1c35b71b9ab849c5d0bf6319e229243048c49970
MohammadAmin-Mashayekhan <mashayekhan.mohammadamin@gmail.come>
2021-12-05 15:06:26 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+function App() {
+  return (
+    <>
+
+      <footer>
+        Guided by:<span> Digital Solutions Master</span>
+        <br />
+        Designed by <a target="_blank" rel="noreferrer" href='https://github.com/MohammadAmin-Mashayekhan'>Mohammad amin Mashayekhan</a>
+      </footer>
+    </>
+
+    
+  );
+}
+
+export default App;
